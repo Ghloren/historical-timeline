af81661720e88aec64a05aa2a0b535f6bdc0b75d
Ghloren <mirowoi1995@gmail.com>
2025-10-22 10:24:28 +0300

    Setup project: Webpack, TypeScript, SASS, Swiper, GSAP

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"), // Папка для статических файлов
+    },
+    compress: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // Путь к твоему index.html в папке src
+    }),
+  ],
+};
